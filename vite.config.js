c91e795255c619b7bff962275f748917706edc5e
ZHEE <tatassembara11@gmail.com>
2025-09-23 14:08:54 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
